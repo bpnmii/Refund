a8ed8ee9159843ee6ad95586fd61392bcb1c96b4
Milena <milena.b.p.nascimento@gmail.com>
2025-07-30 14:58:46 -0300

    expense commit

--- refund-template-main/script.js
+++ refund-template-main/script.js
@@ -38,4 +38,18 @@ form.onsubmit = (event) => {
         amount: amount.value,
         created_at: new Date(),
     }
+
+
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense){
+    try{
+       
+        const expenseItem = document.createElement ("li")
+        expenseItem.classList.add("expense")
+    } catch (error){
+        alert("Não foi possível atualizar a lista de despesas.")
+        console.log(error)
+    }
 }
